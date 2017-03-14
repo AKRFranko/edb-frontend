a9a304d5dcea7e5156603eb03173d7a6901cdbcb
franko <franko@akr.club>
2017-03-14 12:45:02 -0400

    update

--- index.php
+++ index.php
@@ -9,7 +9,11 @@ $current_user = wp_get_current_user();
 if ( !(is_user_logged_in()) ){
   $current_user = null;
 }
+
+ob_start('wp_head');
+$buffer = ob_end_flush();
 $json_user = json_encode( $current_user );
-$user_embed = "<script type=\"text/javascript\">window.CurrentUser= $json_user;</script>";
+$user_embed = "$buffer<script type=\"text/javascript\">window.CurrentUser= $json_user;</script>";
 
 echo str_replace($token,"$token_replacement\n$user_embed",$html);
+
