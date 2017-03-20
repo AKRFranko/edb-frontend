120ae802ed3faf88c40ff42723299d3ae9f690ba
franko <franko@akr.club>
2017-03-20 10:20:23 -0400

    update

--- edb-polymer.js
+++ edb-polymer.js
@@ -166,7 +166,7 @@
       console.log(error);
     },
     __getSelectedItem: function(id) {
-      
+      console.log('getSelectedItem', id );
       if (id == 'new' || !id) {
         this.__unloadItem();
       } else {
@@ -182,8 +182,7 @@
       this.app.addEventListener('product-changed', function( e ){
         this.debounce('selectedIdChanged', function(){
           this.set('selectedId', appRef[refName].selectedId );  
-        }.bind(this))
-
+        }.bind(this));
       }.bind(this));
       this.refreshItems = function() {
         this.set('loading', true );
