5ea0f84b22b7c059c80663a040fe381a2c5581dd
franko <franko@akr.club>
2017-03-26 09:47:53 -0400

    update

--- edb-checkout.js
+++ edb-checkout.js
@@ -1,10 +1,41 @@
 (function(){
   
+  var getToken = function(){ return (Math.random()*Date.now() + '' +Date.now()).toString(24) }
+  console.log( getToken(),getToken(),getToken(),getToken() )
   var Cart = function( user ){
     if(user){
-      // if(user.)
+      this.user = user;
+    }else{
+      this.user = {
+        name: 'guest',
+        billing_address_1:'',
+        billing_address_2:'',
+        billing_city:'',
+        billing_company:'',
+        billing_country:'',
+        billing_email:'',
+        billing_first_name:'',
+        billing_last_name:'',
+        billing_phone:'',
+        billing_postcode:'',
+        billing_state:'',
+        shipping_address_1:'',
+        shipping_address_2:'',
+        shipping_city:'',
+        shipping_company:'',
+        shipping_country:'',
+        shipping_first_name:'',
+        shipping_last_name:'',
+        shipping_postcode:'',
+        shipping_state:''
+      }
     }
+    var items = [];
+    
+    return this;
   }
+  
+  
   
   
 })()
