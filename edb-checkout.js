e0875611c489fffbe89af6fd50f2f32c1bfcef35
franko <franko@akr.club>
2017-03-27 10:34:16 -0400

    update

--- edb-checkout.js
+++ edb-checkout.js
@@ -178,27 +178,10 @@
 
     
 
-    console.log('Catalog Loaded!',Object.keys(Catalog).length, 'entries.' );
-    
-    if(catalogCallback){
-      var entries = Object.keys(Catalog).map( function( k ){ return Catalog[k]; });
-      console.log('Catalog', entries );
-      catalogCallback( entries );
-      entries.forEach( function( e ){
-        var pid = e.product.id;
-        
-        e.variations.forEach( function( v  ){
-            var attrs = v.attributes.reduce( function( o, a){
-                o[a.name]=a.option;
-                return o;
-            }, {} )
-            console.log(pid, attrs )  
-        });  
-        
-        
-        
-      });
-    }
+    var entries = Object.keys(Catalog).map( function( k ){ return Catalog[k]; });
+    console.log('Catalog loaded with %s items',entries.length);
+    catalogCallback( entries );
+    runTest( entries );
     
   }
   
@@ -235,87 +218,30 @@
     delete Cart[token];
   };
 
-
-  // var genSlug = function(){ return (Math.random()*Date.now() + '' +Date.now()).toString(24) }
-
-  // var Customer = function( user ){
-  //   var customer = this;
-  //   var defaultValues = {
-  //                         name: 'guest',
-  //                         billing_address_1:'',
-  //                         billing_address_2:'',
-  //                         billing_city:'',
-  //                         billing_company:'',
-  //                         billing_country:'',
-  //                         billing_email:'',
-  //                         billing_first_name:'',
-  //                         billing_last_name:'',
-  //                         billing_phone:'',
-  //                         billing_postcode:'',
-  //                         billing_state:'',
-  //                         shipping_address_1:'',
-  //                         shipping_address_2:'',
-  //                         shipping_city:'',
-  //                         shipping_company:'',
-  //                         shipping_country:'',
-  //                         shipping_first_name:'',
-  //                         shipping_last_name:'',
-  //                         shipping_postcode:'',
-  //                         shipping_state:''
-  //                       };
-  //   Object.keys( user ).reduce( function( instance, key ){
-  //     instance[key]=user[key];
-  //     return instance;
-  //   }, this  )
-  //   Object.keys(defaultValues).reduce( function( instance, key ){
-  //     if(!instance[key]){
-  //       instance[key]=  defaultValues[key];
-  //     }
-  //     return instance;
-  //   }, this );
-  // };
-
-  // var Cart = function(  ){
-  //   var cart = this;
-
-  //   var items = {};
-  //   cart.addItem = function( prod, qty ){
-  //     var token = genSlug();
-  //     items[token]={ cgenSlug: token, product: prod, quantity: qty };
-  //     return token;
-  //   }
-  //   cart.updateItem = function( token, data ){
-  //     if(!items[token]) return;
-  //     Object.keys(data).forEach( function( key ){
-  //       items[token][key]=data[key];
-  //     });
-  //   }
-  //   cart.removeItem = function( token ){
-  //     delete items[token];
-  //   }
-  //   cart.getItems = function(){
-  //     return Object.keys(items).map( function( token){
-  //       var item = items[token];
-  //     });
-  //   }
-  //   cart.eachItem = function( fn ){
-  //     return Object.keys(items).map( function( token ){
-  //       return fn.call( null, items[token] );
-  //     });
-  //   }
-  //   return cart;
-  // };
-
-
-  // var Checkout = {};
-
-
-
-  // var Calc = {};
-  // Calc.cartItem = function( item ){}
-  // Calc.cartItem = function( item ){}
-
-
+  
+  function runTest(entries){
+     
+    if(catalogCallback){
+      
+      entries.forEach( function( e ){
+        var pid = e.product.id;
+        
+        e.variations.forEach( function( v  ){
+            var attrs = v.attributes.reduce( function( o, a){
+                o[a.name]=a.option;
+                return o;
+            }, {} );
+            console.log(e.name);
+            console.log('', Checkout.getStock(pid, attrs) );
+            console.log('______');
+            // console.log( )  
+        });  
+        
+        
+        
+      });
+    }
+  }
   EDB.Checkout = Checkout;
 
 })()
