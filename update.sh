d99b45aa178d53ebe01166069c3de0dad4a1a91d
franko <franko@akr.club>
2017-03-29 08:25:15 -0400

    update

--- update.sh
+++ update.sh
@@ -1,3 +1,2 @@
 touch readme.txt
-git add . && git commit -am "update" && git push
-ssh franko@97.107.133.7 './refresh-admin.sh'
+git add . && git commit -am "update" && git push && ssh franko@97.107.133.7 './refresh-admin.sh'
