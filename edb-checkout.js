88b61c93280b476dc38bdd0992ef2ece819e62d2
franko <franko@akr.club>
2017-03-29 09:37:13 -0400

    update

--- edb-checkout.js
+++ edb-checkout.js
@@ -111,29 +111,29 @@
       
       app.set('customer',Customer);
       
-      app.set('cart', [] );
-      app.set('catalog', []);
-      // app.set('cart', Object.keys(Cart).map(function(uuid) {
-      //   return Cart[uuid];
-      // }));
+      // app.set('cart', [] );
+      // app.set('catalog', []);
+      app.set('cart', Object.keys(Cart).map(function(uuid) {
+        return Cart[uuid];
+      }));
 
-      // app.set('catalog', Object.keys(Catalog).map(function(uuid) {
-      //   return Catalog[uuid];
-      // }));
+      app.set('catalog', Object.keys(Catalog).map(function(uuid) {
+        return Catalog[uuid];
+      }));
 
       app.get('cart').forEach(function(item, index) {
         Object.keys(item).forEach(function(k) {
-          app.set('cart.' + index + '.' + k, item[k]);
+          app.notifyPath('cart.' + index + '.' + k, item[k]);
         });
       });
 
       app.get('catalog').forEach(function(item, index) {
         Object.keys(item).forEach(function(k) {
-          app.set('catalog.' + index + '.' + k, item[k]);
+          app.notifyPath('catalog.' + index + '.' + k, item[k]);
         });
       });
       
-      console.log('updateApp', app.get('cart'));
+      // console.log('updateApp', app.get('cart'));
 
     })
 
