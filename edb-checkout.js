aabd041b696f902a127602fcdeef2fc540584a9e
franko <franko@akr.club>
2017-04-05 10:04:42 -0400

    update

--- edb-checkout.js
+++ edb-checkout.js
@@ -261,7 +261,7 @@
         });
       } else {
         if (product.variations.length == 0) {
-          console.log(product.meta_box)
+          console.log(product)
           // console.log(product.name, 'has no variations but has attributes: ', product.attributes);
         }
         Products[product.id] = product;
