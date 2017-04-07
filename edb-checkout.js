dbd801ee4437ca6c74afc535f021c499e9a08c1c
franko <franko@akr.club>
2017-04-07 07:58:01 -0400

    update

--- edb-checkout.js
+++ edb-checkout.js
@@ -475,7 +475,8 @@
     if(!entry){
       uuid = productId + ';';
       entry = Blackboard[uuid];
-
+      console.log('uuid???', uuid, Object.keys(Blackboard));
+      
       return entry && entry.group ? entry : null;
     }
     return entry;
