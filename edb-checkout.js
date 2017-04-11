4a3c784030a1416bc8f21c46e80b5c888f01b34d
franko <franko@akr.club>
2017-04-11 16:37:38 -0400

    update

--- edb-checkout.js
+++ edb-checkout.js
@@ -224,7 +224,7 @@
   Checkout.computeCartTotals = function() {
     var subTotal = 0;
     var shippingClass= getShippingClassForCart();
-    var shippingzone = Checkout.getZone();
+    var shippingZzone = Checkout.getZone();
     var lines = [];
     Object.keys(Cart).forEach(function(uuid) {
       var total = Cart[uuid].quantity * Checkout.getPrice(Cart[uuid].product.id, Cart[uuid].variation.attributes);
