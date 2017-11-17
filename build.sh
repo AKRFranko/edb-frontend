646006bd324d1b014c3cbd64d188cd6c639ecfb3
franko <franko@akr.club>
2017-11-17 15:14:10 -0500

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 polymer build --entrypoint index.html
-cp ./index.php ./build/unbundled/
-cp ./style.css ./build/unbundled/
-cd ./build && zip -r unbundled.zip ./unbundled
-cd ../ && scp ./build/unbundled.zip franko@97.107.133.7:~/ 
-ssh franko@97.107.133.7 './refresh-admin.sh'
+cp ./index.php ./build/es5-bundled/
+cp ./style.css ./build/es5-bundled/
+cd ./build && zip -r es5-bundled.zip ./es5-bundled
+ssh franko@97.107.133.7 'rm ~/es5-bundled.zip'
+ssh franko@97.107.133.7 'rm -rf ~/es5-bundled'
+cd ../ && scp ./build/es5-bundled.zip franko@97.107.133.7:~/ 
+ssh franko@97.107.133.7 './refresh-bundled.sh'
